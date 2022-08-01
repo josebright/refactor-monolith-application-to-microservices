30a204da0531b83b42201e92978a44a5e3662201
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2022-08-01 14:03:06 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Ugwumadu
-export POSTGRES_HOST=database-1.ccf3xc244sdd.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=myusername
+export POSTGRES_PASSWORD=mypasssword
+export POSTGRES_HOST=database-1.*********.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=myawsbucket-133214380233-dev
+export AWS_BUCKET=postgrespassword
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
