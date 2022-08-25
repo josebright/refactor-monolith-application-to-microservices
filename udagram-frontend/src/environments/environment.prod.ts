f34f8a908dd8079e10b9a25fd136f0d261653338
unknown <josebright29@gmail.com>
2022-08-25 12:13:15 +0000

    final commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9bc10e042ce34bfb889af8e5fca30bf-586843023.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a03b49f0b468c46bb846e87f26cf641d-2146548206.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
